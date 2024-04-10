5bf60dff3bccce2eee1772a7b974a0768384a761
kavern1128 <kavern@qq.com>
2024-04-10 08:19:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,9 +7,25 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #============================================================
+# 移除要替换的包
+rm -rf feeds/packages/net/v2ray-geodata
 
-# Modify default IP
+# 添加额外插件
+git clone -b js --single-branch https://github.com/gngpp/luci-theme-design package/luci-theme-design
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+echo "
+# 主题
+CONFIG_PACKAGE_luci-theme-design=y
+
+# mosdns
+CONFIG_PACKAGE_luci-app-mosdns=y
+
+" >> .config
+
+# 修改默认IP
 # sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
-# Modify hostname
-# sed -i 's/OpenWrt/Newifi-D1/g' package/base-files/files/bin/config_generate
+# 修改主机名
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
